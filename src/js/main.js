7359c36e01d0f47d0545d60ce1aa511bb07e19f9
Petropavlovskiy Artiom <artpetropavlovskij@gmail.com>
2026-08-10 18:31:31 +0500

    init

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+
+import { Fancybox } from "@fancyapps/ui/dist/fancybox/";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+import 'swiper/css';
+
+Fancybox.bind("[data-fancybox]", {
+  dragToClose: false,
+});
