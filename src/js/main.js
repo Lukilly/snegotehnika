6f72a2acf6031e14f7de6f0a473f413fb7f2d844
Lucky <glebplotnickov@gmail.com>
2026-08-12 13:14:07 +0500

    перекинул хедер и футер, сделал cart, и боковое меню

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,141 @@ import 'swiper/css';
 Fancybox.bind("[data-fancybox]", {
   dragToClose: false,
 });
+
+
+// side-navigation
+const sideNavigationBtn = document.querySelector('.side__navigation__close')
+const sideNavigationContent = document.querySelector('.side__navigation')
+sideNavigationBtn.addEventListener('click', () => {
+  sideNavigationContent.classList.toggle('hide')
+})
+
+
+
+// Cart
+let cartCount = 0;
+const cartCountElement = document.querySelector('.cart-count')
+const addToCartButton = document.querySelector('.catalog__toggle')
+addToCartButton.addEventListener('click', () => {
+  cartCount++;
+  cartCountElement.textContent = cartCount
+});
+
+
+
+// Menu
+const Menu = document.querySelector('.header__menu')
+const MenuBtn = document.querySelector('.header__menu__btn')
+const MenuSidebar = document.querySelector('.header__menu__sidebar')
+
+MenuBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
+
+  MenuBtn.classList.toggle('active');
+  MenuSidebar.classList.toggle('active');
+
+  CatalogBurger.classList.remove('active');
+  CatalogInner.classList.remove('active');
+
+});
+
+document.addEventListener('click', (e) => {
+  if (!Menu.contains(e.target)) {
+    MenuBtn.classList.remove('active');
+    MenuSidebar.classList.remove('active');
+  }
+});
+
+
+
+// Catalog
+const CatalogBurger = document.querySelector('.catalog__toggle');
+const CatalogInner = document.querySelector('.catalog__dropdown')
+const CatalogMoreBtns = document.querySelectorAll('.catalog__item__btn');
+const CatalogMore = document.querySelector('.catalog__content');
+const Catalog = document.querySelector('.catalog')
+const items = document.querySelectorAll('.catalog__item');
+const panels = document.querySelectorAll('.catalog__panel');
+// Открытие каталога
+CatalogBurger.addEventListener('click', () => {
+  CatalogBurger.classList.toggle('active');
+  CatalogInner.classList.toggle('active');
+
+  MenuBtn.classList.remove('active');
+  MenuSidebar.classList.remove('active');
+});
+// Закрытие каталога при нажатии вне каталога
+document.addEventListener("click", (e) =>{
+  if (!Catalog.contains(e.target)) {
+    CatalogBurger.classList.remove('active');
+    CatalogInner.classList.remove('active');
+    CatalogMoreBtns.forEach(btn => {
+      btn.classList.remove('active');
+    });
+  }
+});
+
+function openMobile(item) {
+const id = item.dataset.category;
+const panel = document.getElementById(id);
+if (item.classList.contains("active")) {
+    item.classList.remove("active");
+    panel.classList.remove("active");
+    CatalogMore.append(panel);
+    items.forEach(el => {
+        el.classList.remove("hidden");
+    });
+    return;
+}
+items.forEach(el => {
+    el.classList.remove("active");
+    el.classList.remove("hidden");
+});
+panels.forEach(el => {
+    el.classList.remove("active");
+});
+item.classList.add("active");
+panel.classList.add("active");
+item.after(panel);
+items.forEach(el => {
+    if (el !== item) {
+        el.classList.add("hidden");
+    }
+});
+
+}
+
+function openDesktop(item) {
+  const id = item.dataset.category;
+  const panel = document.getElementById(id);
+  items.forEach(el => el.classList.remove("active"));
+  panels.forEach(el => el.classList.remove("active"));
+  item.classList.add("active");
+  CatalogMore.classList.add("active");
+  panel.classList.add("active");
+}
+
+items.forEach(item => {
+  item.addEventListener("mouseenter", () => {
+    if (window.innerWidth > 992) {
+      openDesktop(item);
+    }
+  })
+
+  item.addEventListener("click", () => {
+    if (window.innerWidth <= 992) {
+      openMobile(item);
+    }
+  })
+})
+
+
+// Scroll__footer
+const scrollTopButton = document.querySelector('[data-scroll-top]');
+
+scrollTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
