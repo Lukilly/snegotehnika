0a91e6a427f097a8795a950d5e148f0804fdac46
Lucky <glebplotnickov@gmail.com>
2026-09-09 03:41:30 +0500

    contacts finish

--- vite.config.js
+++ vite.config.js
@@ -81,6 +81,7 @@ export default defineConfig({
         cart: resolve(__dirname, "pages/cart.html"),
         delivery: resolve(__dirname, "pages/delivery.html"),
         wholesale: resolve(__dirname, "pages/wholesale.html"),
+        contacts: resolve(__dirname, "pages/contacts.html"),
         // contacts: resolve(__dirname, "pages/contacts.html"),
       },
       output: {
